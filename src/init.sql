c06a338a080525019d1c3d520525f66d8ed6dd41
Rahul Joshi <48386687+17J@users.noreply.github.com>
2024-05-08 14:54:54 +0530

    Update init.sql

--- src/init.sql
+++ src/init.sql
@@ -1,4 +1,3 @@
-```sql
     CREATE DATABASE bugtracker;
     USE bugtracker;
     CREATE TABLE status
@@ -60,4 +59,4 @@ INSERT INTO bugtracker.item VALUES (1, 'Implement Project Apollo', 'Implement th
 INSERT INTO bugtracker.item VALUES (2, 'Project Apollo: PIR', 'Conduct a PIR of Project Apollo', 2, 2, 2);
 INSERT INTO bugtracker.item VALUES (3, 'Define tasks for Project Metro', 'Define project plan and Roadmap', 4, 2, 3);
 INSERT INTO bugtracker.item VALUES (4, 'Fix website bug', 'Test website using different browsers', 3, 4, 4);
-```
+
